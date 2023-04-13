1da9d5502ab0beb47c1a7ed5ce6c625a082dc566
toshokan <toshokan@shojigate.net>
2023-04-13 03:43:01 -0400

    feat: http tracker

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
 pub mod bencode;
+mod server;
+
+pub async fn start() {
+    server::start().await;
+}
---
+++ src/server.rs
@@ -0,0 +1,37 @@
+use axum::Router;
+use axum::extract::Query;
+use axum::routing::get;
+
+#[derive(Debug)]
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Event {
+    Started,
+    Completed,
+    Stopped
+}
+
+#[derive(Debug)]
+#[derive(serde::Deserialize)]
+struct AnnounceRequest {
+    info_hash: String,
+    peer_id: String,
+    ip: Option<String>,
+    port: u16,
+    uploaded: usize,
+    left: usize,
+    event: Option<Event>
+}
+
+async fn announce(Query(announce): Query<AnnounceRequest>) {
+}
+
+pub async fn start() {
+    let app = Router::new()
+        .route("/announce", get(announce));
+
+    axum::Server::bind(&([127, 0, 0, 1], 8001).into())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
