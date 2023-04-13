a16d9f58c86e24c23edecce989590dfc55a4dae0
toshokan <toshokan@shojigate.net>
2023-04-13 15:58:46 -0400

    feat: BEP 23 compact peer list

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,7 +1,10 @@
+use std::net::Ipv4Addr;
+
 use axum::extract::Query;
 use axum::routing::get;
 use axum::Router;
 
+use crate::bencode::{encode, Value};
 use crate::types::Event;
 
 #[derive(Debug, serde::Deserialize)]
@@ -13,9 +16,59 @@ struct AnnounceRequest {
     uploaded: usize,
     left: usize,
     event: Option<Event>,
+    compact: Option<u8>,
 }
 
-async fn announce(Query(announce): Query<AnnounceRequest>) {}
+struct Peer {
+    peer_id: String,
+    ip: Ipv4Addr,
+    port: u16,
+}
+
+async fn announce(Query(announce): Query<AnnounceRequest>) -> String {
+    let peers: Vec<Peer> = vec![];
+
+    if announce.compact.unwrap_or(1) == 1 {
+        // BEP 23 Compact representation
+        use bytes::{BufMut, BytesMut};
+        use std::collections::BTreeMap;
+
+        let mut peer_string = BytesMut::new();
+        for peer in peers.into_iter() {
+            let ip_bytes: u32 = peer.ip.into();
+            peer_string.put_u32(ip_bytes);
+            peer_string.put_u16(peer.port)
+        }
+        let peers = std::str::from_utf8(&peer_string).unwrap().to_string();
+
+        let mut data = BTreeMap::new();
+        data.insert("interval".to_string(), Value::Int(30));
+        data.insert("peers".to_string(), Value::String(peers));
+
+        encode(&Value::Dict(data))
+    } else {
+        // BEP 3 representation
+        use std::collections::BTreeMap;
+
+        let peer_dicts = peers
+            .into_iter()
+            .map(|p| {
+                let mut data = BTreeMap::new();
+                data.insert("peer id".to_string(), Value::String(p.peer_id.clone()));
+                data.insert("ip".to_string(), Value::String(p.ip.to_string()));
+                data.insert("port".to_string(), Value::Int(p.port as i32));
+
+                Value::Dict(data)
+            })
+            .collect();
+
+        let mut data = BTreeMap::new();
+        data.insert("interval".to_string(), Value::Int(30));
+        data.insert("peers".to_string(), Value::List(peer_dicts));
+
+        encode(&Value::Dict(data))
+    }
+}
 
 pub async fn start() {
     let app = Router::new().route("/announce", get(announce));
