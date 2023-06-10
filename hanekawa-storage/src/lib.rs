c926ff004e76711752e9026b2791e71c590bf8b2
toshokan <toshokan@shojigate.net>
2023-06-10 17:56:44 -0400

    feat(logging): change sqlx statement log level to trace

--- hanekawa-storage/src/lib.rs
+++ hanekawa-storage/src/lib.rs
@@ -1,5 +1,7 @@
 use hanekawa_common::Config;
-use sqlx::postgres::PgPoolOptions;
+
+use sqlx::postgres::{PgConnectOptions, PgPoolOptions};
+use sqlx::ConnectOptions;
 
 pub mod info_hash;
 pub mod peer;
@@ -11,11 +13,17 @@ pub struct Services {
 
 impl Services {
     pub async fn start(cfg: &Config) -> Self {
+        let mut connect_options: PgConnectOptions = cfg.database_url.parse().unwrap();
+
+        connect_options.log_statements(log::LevelFilter::Trace);
+
         let pool = PgPoolOptions::new()
             .max_connections(80)
-            .connect(&cfg.database_url)
+            .connect_with(connect_options)
             .await
             .unwrap();
+
+        sqlx::migrate!().run(&pool).await.unwrap();
 
         let peer = peer::PeerRepository::new(pool.clone(), cfg);
         let info_hash = info_hash::InfoHashRepository::new(pool);
